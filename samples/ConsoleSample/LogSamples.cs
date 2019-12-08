0f43f01935cb081476539ded5926a15880749779
David Zidar <david.zidar@gmail.com>
2019-12-09 00:05:14 +0100

    Refactor ConsoleSample

---
+++ samples/ConsoleSample/LogSamples.cs
@@ -0,0 +1,115 @@
+using Figgle;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+
+namespace ConsoleSample
+{
+	public class LogSamples
+	{
+		private readonly ILogger logger;
+
+		public LogSamples(ILogger logger)
+		{
+			this.logger = logger;
+		}
+
+		public void LogAll()
+		{
+			LogIntro();
+			LogSimpleExamples();
+			LogComplexExamples();
+			LogAllLevels();
+			LogException();
+		}
+
+		public void LogIntro()
+		{
+			logger.LogInformation(new EventId(0, "Intro"), Environment.NewLine + FiggleFonts.Slant.Render("TinyLogger"));
+		}
+
+		public void LogSimpleExamples()
+		{
+			var eventId = new EventId(0, "SimpleExamples");
+
+			logger.LogInformation(eventId, "Example {number}", 1234);
+			logger.LogInformation(eventId, "Example {string}", "foobar");
+			logger.LogInformation(eventId, "Example {datetime}", DateTime.UtcNow);
+			logger.LogInformation(eventId, "Example {datetime:o}", DateTime.UtcNow);
+			logger.LogInformation(eventId, "Example {guid}", Guid.NewGuid());
+			logger.LogInformation(eventId, "Example {url}", new Uri("https://www.example.com/"));
+		}
+
+		public void LogComplexExamples()
+		{
+			var eventId = new EventId(0, "ComplexExamples");
+
+			logger.LogInformation(
+				eventId,
+				"Example list {list}",
+				new List<object>
+				{
+					1234,
+					"example string",
+					new Uri("https://www.example.com/")
+				}
+			);
+
+			logger.LogInformation(
+				eventId,
+				"Example dictionary {dictionary}",
+				new Dictionary<string, object>
+				{
+					{ "someValue", 1234 },
+					{ "text", "lorem ipsum" },
+					{ "timestamp", DateTime.UtcNow }
+				}
+			);
+		}
+
+		public void LogAllLevels()
+		{
+			var eventId = new EventId(0, "LogLevels");
+
+			var logLevels = new[]
+			{
+				LogLevel.Trace,
+				LogLevel.Debug,
+				LogLevel.Information,
+				LogLevel.Warning,
+				LogLevel.Error,
+				LogLevel.Critical
+			};
+
+			foreach (var logLevel in logLevels)
+			{
+				logger.Log(logLevel, eventId, "Example message with log level {logLevel}", logLevel);
+			}
+		}
+
+		public void LogException()
+		{
+			var eventId = new EventId(0, "Exceptions");
+
+			try
+			{
+				InternalMethodA();
+			}
+			catch (Exception ex)
+			{
+				logger.LogWarning(eventId, ex, "Caught example exception");
+				logger.LogError(eventId, ex, "Caught example exception");
+			}
+
+			void InternalMethodA()
+			{
+				InternalMethodB();
+			}
+
+			void InternalMethodB()
+			{
+				throw new InvalidOperationException("Example error message");
+			}
+		}
+	}
+}
---
+++ samples/ConsoleSample/SampleExceptionExtender.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using TinyLogger;
+
+namespace ConsoleSample
+{
+	public class SampleExceptionExtender : ILogExtender
+	{
+		public void Extend(Dictionary<string, object> data)
+		{
+			if (data.ContainsKey("exception") && data["exception"] is Exception exception)
+			{
+				exception.Demystify();
+			}
+		}
+	}
+}
