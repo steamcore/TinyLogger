1eceaddc3da7fda7b8ac1c1a28c6788589005a7c
David Zidar <david.zidar@gmail.com>
2019-12-09 01:38:24 +0100

    Improve performance of console renderer by using ANSI color sequences

--- src/TinyLogger/Console/ConsoleRenderer.cs
+++ src/TinyLogger/Console/ConsoleRenderer.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Runtime.InteropServices;
+using System.Text;
 using System.Threading.Tasks;
 using SystemConsole = System.Console;
 
@@ -9,7 +11,7 @@ namespace TinyLogger.Console
 	/// </summary>
 	public class ConsoleRenderer : ILogRenderer
 	{
-		private readonly IConsoleTheme theme;
+		private readonly Action<TokenizedMessage> renderMessage;
 
 		public ConsoleRenderer()
 			: this(new DefaultConsoleTheme())
@@ -18,59 +20,214 @@ namespace TinyLogger.Console
 
 		public ConsoleRenderer(IConsoleTheme theme)
 		{
-			this.theme = theme;
+			renderMessage = SelectRenderMethod();
+
+			Action<TokenizedMessage> SelectRenderMethod()
+			{
+				// Honor no-color.org
+				if (Environment.GetEnvironmentVariable("NO_COLOR") != null)
+				{
+					return RenderPlainText;
+				}
+
+				// Try to enable VT-processing on Windows
+				if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+				{
+					var handle = NativeMethods.GetStdHandle(NativeMethods.STD_OUTPUT_HANDLE);
+
+					// Fallback to slow classic colors if console mode can't be accessed
+					if (!NativeMethods.GetConsoleMode(handle, out var consoleMode))
+					{
+						return message => RenderWithClassicColors(theme, message);
+					}
+
+					if ((consoleMode & NativeMethods.ENABLE_VIRTUAL_TERMINAL_PROCESSING) == 0)
+					{
+						NativeMethods.SetConsoleMode(handle, consoleMode | NativeMethods.ENABLE_VIRTUAL_TERMINAL_PROCESSING);
+					}
+				}
+
+				return message => RenderWithAnsiColors(theme, message);
+			}
 		}
 
 		public Task Render(Func<TokenizedMessage> message)
 		{
-			RenderMessage(theme, message());
+			renderMessage(message());
 			return Task.CompletedTask;
 		}
 
-		private static void RenderMessage(IConsoleTheme theme, TokenizedMessage message)
+		private static void RenderPlainText(TokenizedMessage message)
+		{
+			var sb = new StringBuilder(128);
+
+			foreach (var token in message.Message)
+			{
+				sb.Append(token.ToString());
+			}
+
+			SystemConsole.Write(sb.ToString());
+		}
+
+		private static void RenderWithAnsiColors(IConsoleTheme theme, TokenizedMessage message)
+		{
+			const string Reset = "\x1b[0m";
+
+			var sb = new StringBuilder(128);
+
+			foreach (var token in message.Message)
+			{
+				if (token.Type != MessageTokenType.ObjectToken)
+				{
+					sb.Append(token.ToString());
+					continue;
+				}
+
+				switch (theme.GetColors(token.Value, message.LogLevel))
+				{
+					case (ConsoleColor foreground, ConsoleColor background):
+						sb.Append("\x1b[");
+						sb.Append(GetForeground(foreground));
+						sb.Append(';');
+						sb.Append(GetBackground(background));
+						sb.Append('m');
+						sb.Append(token.ToString());
+						sb.Append(Reset);
+						break;
+
+					case (ConsoleColor foreground, _):
+						sb.Append("\x1b[");
+						sb.Append(GetForeground(foreground));
+						sb.Append('m');
+						sb.Append(token.ToString());
+						sb.Append(Reset);
+						break;
+
+					case (_, ConsoleColor background):
+						sb.Append("\x1b[");
+						sb.Append(GetBackground(background));
+						sb.Append('m');
+						sb.Append(token.ToString());
+						sb.Append(Reset);
+						break;
+
+					default:
+						sb.Append(token.ToString());
+						break;
+				}
+			}
+
+			SystemConsole.Write(sb.ToString());
+
+			static string GetForeground(ConsoleColor color)
+			{
+				return color switch
+				{
+					ConsoleColor.Black => "30",
+					ConsoleColor.DarkRed => "31",
+					ConsoleColor.DarkGreen => "32",
+					ConsoleColor.DarkYellow => "33",
+					ConsoleColor.DarkBlue => "34",
+					ConsoleColor.DarkMagenta => "35",
+					ConsoleColor.DarkCyan => "36",
+					ConsoleColor.Gray => "37",
+
+					ConsoleColor.DarkGray => "90",
+					ConsoleColor.Red => "91",
+					ConsoleColor.Green => "92",
+					ConsoleColor.Yellow => "93",
+					ConsoleColor.Blue => "94",
+					ConsoleColor.Magenta => "95",
+					ConsoleColor.Cyan => "96",
+					ConsoleColor.White => "97",
+
+					_ => "39"
+				};
+			}
+
+			static string GetBackground(ConsoleColor color)
+			{
+				return color switch
+				{
+					ConsoleColor.Black => "40",
+					ConsoleColor.DarkRed => "41",
+					ConsoleColor.DarkGreen => "42",
+					ConsoleColor.DarkYellow => "43",
+					ConsoleColor.DarkBlue => "44",
+					ConsoleColor.DarkMagenta => "45",
+					ConsoleColor.DarkCyan => "46",
+					ConsoleColor.Gray => "47",
+
+					ConsoleColor.DarkGray => "100",
+					ConsoleColor.Red => "101",
+					ConsoleColor.Green => "102",
+					ConsoleColor.Yellow => "103",
+					ConsoleColor.Blue => "104",
+					ConsoleColor.Magenta => "105",
+					ConsoleColor.Cyan => "106",
+					ConsoleColor.White => "107",
+
+					_ => "49"
+				};
+			}
+		}
+
+		private static void RenderWithClassicColors(IConsoleTheme theme, TokenizedMessage message)
 		{
 			foreach (var token in message.Message)
 			{
-				RenderToken(theme, message, token);
+				ConsoleColor? foreground = null;
+				ConsoleColor? background = null;
+
+				if (token.Type == MessageTokenType.ObjectToken)
+				{
+					(foreground, background) = theme.GetColors(token.Value, message.LogLevel);
+				}
+
+				RenderToken(token.ToString(), foreground, background);
+			}
+
+			static void RenderToken(string message, ConsoleColor? foreground = null, ConsoleColor? background = null)
+			{
+				try
+				{
+					if (background != null)
+					{
+						SystemConsole.BackgroundColor = background.Value;
+					}
+
+					if (foreground != null)
+					{
+						SystemConsole.ForegroundColor = foreground.Value;
+					}
+
+					SystemConsole.Write(message);
+				}
+				finally
+				{
+					if (background != null || foreground != null)
+					{
+						SystemConsole.ResetColor();
+					}
+				}
 			}
 		}
 
-		private static void RenderToken(IConsoleTheme theme, TokenizedMessage message, MessageToken token)
+		private static class NativeMethods
 		{
-			ConsoleColor? foreground = null;
-			ConsoleColor? background = null;
+			public const int STD_OUTPUT_HANDLE = -11;
+			public const int STD_ERROR_HANDLE = -12;
 
-			if (token.Type == MessageTokenType.ObjectToken)
-			{
-				(foreground, background) = theme.GetColors(token.Value, message.LogLevel);
-			}
+			public const uint ENABLE_VIRTUAL_TERMINAL_PROCESSING = 0x0004;
 
-			RenderToken(token.ToString(), foreground, background);
-		}
+			[DllImport("kernel32.dll")]
+			public static extern bool GetConsoleMode(IntPtr hConsoleHandle, out uint lpMode);
 
-		private static void RenderToken(string message, ConsoleColor? foreground = null, ConsoleColor? background = null)
-		{
-			try
-			{
-				if (background != null)
-				{
-					SystemConsole.BackgroundColor = background.Value;
-				}
+			[DllImport("kernel32.dll")]
+			public static extern bool SetConsoleMode(IntPtr hConsoleHandle, uint dwMode);
 
-				if (foreground != null)
-				{
-					SystemConsole.ForegroundColor = foreground.Value;
-				}
-
-				SystemConsole.Write(message);
-			}
-			finally
-			{
-				if (background != null || foreground != null)
-				{
-					SystemConsole.ResetColor();
-				}
-			}
+			[DllImport("kernel32.dll", SetLastError = true)]
+			public static extern IntPtr GetStdHandle(int nStdHandle);
 		}
 	}
 }
