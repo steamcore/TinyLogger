466e21540455dd4901bc0586ffd411c2930b3451
David Zidar <david@zidar.se>
2020-08-15 16:08:41 +0200

    Upgrade NuGet dependencies, fix analyzer warnings

--- src/TinyLogger/Console/DefaultConsoleTheme.cs
+++ src/TinyLogger/Console/DefaultConsoleTheme.cs
@@ -5,51 +5,49 @@ namespace TinyLogger.Console
 {
 	public class DefaultConsoleTheme : IConsoleTheme
 	{
-		private static readonly ConsoleColor? none = null;
-
 		public virtual (ConsoleColor? foreground, ConsoleColor? background) GetColors(object? value, LogLevel logLevel)
 		{
 			return (value, logLevel) switch
 			{
-				(bool b, _) => (b ? ConsoleColor.Green : ConsoleColor.Red, none),
+				(bool b, _) => (b ? ConsoleColor.Green : ConsoleColor.Red, null),
 
-				(byte _, _) => (ConsoleColor.Magenta, none),
-				(sbyte _, _) => (ConsoleColor.Magenta, none),
-				(short _, _) => (ConsoleColor.Magenta, none),
-				(ushort _, _) => (ConsoleColor.Magenta, none),
-				(int _, _) => (ConsoleColor.Magenta, none),
-				(uint _, _) => (ConsoleColor.Magenta, none),
-				(long _, _) => (ConsoleColor.Magenta, none),
-				(ulong _, _) => (ConsoleColor.Magenta, none),
-				(float _, _) => (ConsoleColor.Magenta, none),
-				(double _, _) => (ConsoleColor.Magenta, none),
-				(decimal _, _) => (ConsoleColor.Magenta, none),
+				(byte _, _) => (ConsoleColor.Magenta, null),
+				(sbyte _, _) => (ConsoleColor.Magenta, null),
+				(short _, _) => (ConsoleColor.Magenta, null),
+				(ushort _, _) => (ConsoleColor.Magenta, null),
+				(int _, _) => (ConsoleColor.Magenta, null),
+				(uint _, _) => (ConsoleColor.Magenta, null),
+				(long _, _) => (ConsoleColor.Magenta, null),
+				(ulong _, _) => (ConsoleColor.Magenta, null),
+				(float _, _) => (ConsoleColor.Magenta, null),
+				(double _, _) => (ConsoleColor.Magenta, null),
+				(decimal _, _) => (ConsoleColor.Magenta, null),
 
-				(char _, _) _ => (ConsoleColor.Yellow, none),
-				(string _, _) _ => (ConsoleColor.Yellow, none),
+				(char _, _) _ => (ConsoleColor.Yellow, null),
+				(string _, _) _ => (ConsoleColor.Yellow, null),
 
-				(DateTime _, _) _ => (ConsoleColor.Cyan, none),
-				(DateTimeOffset _, _) _ => (ConsoleColor.Cyan, none),
-				(TimeSpan _, _) _ => (ConsoleColor.Cyan, none),
+				(DateTime _, _) _ => (ConsoleColor.Cyan, null),
+				(DateTimeOffset _, _) _ => (ConsoleColor.Cyan, null),
+				(TimeSpan _, _) _ => (ConsoleColor.Cyan, null),
 
-				(Guid _, _) _ => (ConsoleColor.DarkMagenta, none),
+				(Guid _, _) _ => (ConsoleColor.DarkMagenta, null),
 
-				(Uri _, _) _ => (ConsoleColor.Blue, none),
+				(Uri _, _) _ => (ConsoleColor.Blue, null),
 
-				(Exception _, LogLevel.Warning) => (ConsoleColor.Yellow, none),
-				(Exception _, LogLevel.Error) => (ConsoleColor.Red, none),
-				(Exception _, LogLevel.Critical) => (ConsoleColor.Red, none),
+				(Exception _, LogLevel.Warning) => (ConsoleColor.Yellow, null),
+				(Exception _, LogLevel.Error) => (ConsoleColor.Red, null),
+				(Exception _, LogLevel.Critical) => (ConsoleColor.Red, null),
 
-				(EventId _, _) => (ConsoleColor.DarkGray, none),
+				(EventId _, _) => (ConsoleColor.DarkGray, null),
 
-				(LogLevel level, _) when level == LogLevel.Trace => (ConsoleColor.DarkGray, none),
-				(LogLevel level, _) when level == LogLevel.Debug => (ConsoleColor.DarkBlue, none),
-				(LogLevel level, _) when level == LogLevel.Information => (ConsoleColor.DarkGreen, none),
-				(LogLevel level, _) when level == LogLevel.Warning => (ConsoleColor.Yellow, none),
-				(LogLevel level, _) when level == LogLevel.Error => (ConsoleColor.Red, none),
+				(LogLevel level, _) when level == LogLevel.Trace => (ConsoleColor.DarkGray, null),
+				(LogLevel level, _) when level == LogLevel.Debug => (ConsoleColor.DarkBlue, null),
+				(LogLevel level, _) when level == LogLevel.Information => (ConsoleColor.DarkGreen, null),
+				(LogLevel level, _) when level == LogLevel.Warning => (ConsoleColor.Yellow, null),
+				(LogLevel level, _) when level == LogLevel.Error => (ConsoleColor.Red, null),
 				(LogLevel level, _) when level == LogLevel.Critical => (ConsoleColor.White, ConsoleColor.DarkRed),
 
-				(_, _) => (ConsoleColor.White, none)
+				(_, _) => (ConsoleColor.White, null)
 			};
 		}
 	}
