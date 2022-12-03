dfa2fbf4b09982c804b6e41cfe856281f19f1121
David Zidar <david@zidar.se>
2022-12-04 00:28:49 +0100

    Remove pragma warning disable CA1812

--- samples/WebSample/Program.cs
+++ samples/WebSample/Program.cs
@@ -1,5 +1,3 @@
-#pragma warning disable CA1812 // Workaround for analyzer bug https://github.com/dotnet/roslyn-analyzers/issues/5628
-
 var builder = WebApplication.CreateBuilder(args);
 
 // Add TinyLogger, remember to disable the default Console logger in appsettings.json
