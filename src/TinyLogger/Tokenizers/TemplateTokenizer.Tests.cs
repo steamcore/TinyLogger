4b5afeb463a64eb6b0bac9aa72f8fa2985127c34
David Zidar <david@zidar.se>
2024-04-15 21:37:23 +0200

    Fix xUnit test data serialization issue

--- src/TinyLogger/Tokenizers/TemplateTokenizer.Tests.cs
+++ src/TinyLogger/Tokenizers/TemplateTokenizer.Tests.cs
@@ -3,66 +3,59 @@ namespace TinyLogger.Tokenizers;
 public class TemplateTokenizerTests
 {
 	[Theory]
-	[MemberData(nameof(GetTestData))]
-	public void Tokenize_should_return_expected_tokens(string input, IReadOnlyList<MessageToken> expectedTokens)
+	[MemberData(nameof(TestDataKeys))]
+	public void Tokenize_should_return_expected_tokens(string input)
 	{
 		var result = TemplateTokenizer.Tokenize(input);
 
-		result.ShouldBe(expectedTokens);
+		result.ShouldBe(testData[input]);
 	}
 
-	public static TheoryData<string, IReadOnlyList<MessageToken>> GetTestData()
+	public static TheoryData<string> TestDataKeys => new(testData.Keys);
+
+	private static readonly IReadOnlyDictionary<string, IReadOnlyList<MessageToken>> testData = new Dictionary<string, IReadOnlyList<MessageToken>>
 	{
-		return new()
 		{
-			{
-				"abc{{def}ghi",
-				new List<MessageToken>
-				{
-					MessageToken.FromLiteral("abc{{def}ghi")
-				}
-			},
-			{
-				"abc{def}}ghi",
-				new List<MessageToken>
-				{
-					MessageToken.FromLiteral("abc{def}}ghi")
-				}
-			},
-			{
-				"abc{{def}}ghi",
-				new List<MessageToken>
-				{
-					MessageToken.FromLiteral("abc{{def}}ghi")
-				}
-			},
-			{
-				"abc{d ef}ghi",
-				new List<MessageToken>
-				{
-					MessageToken.FromLiteral("abc"),
-					MessageToken.FromObject("d ef"),
-					MessageToken.FromLiteral("ghi")
-				}
-			},
-			{
-				"abc{def}ghi",
-				new List<MessageToken>
-				{
-					MessageToken.FromLiteral("abc"),
-					MessageToken.FromObject("def"),
-					MessageToken.FromLiteral("ghi")
-				}
-			},
-			{
-				"{a,2}{b:o}{c,3:#}",
-				new List<MessageToken>
-				{
-					MessageToken.FromObject("a", alignment: 2),
-					MessageToken.FromObject("b", format: "o"),
-					MessageToken.FromObject("c", alignment: 3, format: "#")
-				}
-			}
-		};
-	}
+			"abc{{def}ghi",
+			[
+				MessageToken.FromLiteral("abc{{def}ghi")
+			]
+		},
+		{
+			"abc{def}}ghi",
+			[
+				MessageToken.FromLiteral("abc{def}}ghi")
+			]
+		},
+		{
+			"abc{{def}}ghi",
+			[
+				MessageToken.FromLiteral("abc{{def}}ghi")
+			]
+		},
+		{
+			"abc{d ef}ghi",
+			[
+				MessageToken.FromLiteral("abc"),
+				MessageToken.FromObject("d ef"),
+				MessageToken.FromLiteral("ghi")
+			]
+		},
+		{
+			"abc{def}ghi",
+			[
+				MessageToken.FromLiteral("abc"),
+				MessageToken.FromObject("def"),
+				MessageToken.FromLiteral("ghi")
+			]
+		},
+		{
+			"{a,2}{b:o}{c,3:#}",
+			[
+				MessageToken.FromObject("a", alignment: 2),
+				MessageToken.FromObject("b", format: "o"),
+				MessageToken.FromObject("c", alignment: 3, format: "#")
+			]
+		}
+	};
 }
