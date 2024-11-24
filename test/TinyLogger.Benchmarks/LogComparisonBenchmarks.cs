b89a22240fba576b2558f4213a43858a67db9e36
David Zidar <david@zidar.se>
2024-11-24 15:02:47 +0100

    Update .editorconfig

--- test/TinyLogger.Benchmarks/LogComparisonBenchmarks.cs
+++ test/TinyLogger.Benchmarks/LogComparisonBenchmarks.cs
@@ -1,4 +1,3 @@
-using System.Diagnostics.CodeAnalysis;
 using BenchmarkDotNet.Attributes;
 using Microsoft.Extensions.Logging;
 using TinyLogger.Console;
@@ -9,7 +8,6 @@ namespace TinyLogger.Benchmarks;
 
 [MemoryDiagnoser]
 [ShortRunJob]
-[SuppressMessage("Maintainability", "CA1515:Consider making public types internal", Justification = "BenchmarkDotNet")]
 public partial class LogComparisonBenchmarks : IDisposable
 {
 	private readonly ILoggerFactory dotnetConsoleFactory;
